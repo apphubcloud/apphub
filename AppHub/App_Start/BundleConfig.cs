01d2ad8e2d2f22bc29ea9ba6dfd22939c70da52b
apphub\Ajay <apphubcloud@gmail.com>
2017-04-26 18:25:34 +0530

    commit for initial setup

---
+++ AppHub/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AppHub
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new ScriptBundle("~/Content/vendor_js").Include(
+                      "~/Content/jquery/jquery.js"));
+
+            bundles.Add(new StyleBundle("~/Content/vendor_css").Include(
+                      "~/Content/slds/assets/styles/salesforce-lightning-design-system.css"));
+        }
+    }
+}
